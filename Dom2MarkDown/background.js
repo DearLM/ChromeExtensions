a5b8f94fb910884ec2b3a33522e9fc407eabb0d2
15140046856 <1150174401@qq.com>
2023-09-01 16:08:44 +0800

    js取消压缩

--- Dom2MarkDown/background.js
+++ Dom2MarkDown/background.js
@@ -1,1 +1,59 @@
-let n=!1;chrome.action.onClicked.addListener(e=>{(n=!n)?(o(e.id),chrome.action.setIcon({path:"md-on.png"})):(c(e.id),chrome.action.setIcon({path:"md-off.png"}))});const t="html2md";function o(e){chrome.contextMenus.create({id:t,title:"复制md",contexts:["all"]}),chrome.tabs.sendMessage(e,{cmd:"open"}),chrome.contextMenus.onClicked.addListener(d)}function c(e){chrome.contextMenus.remove(t),chrome.tabs.sendMessage(e,{cmd:"shutdown"}),chrome.contextMenus.onClicked.removeListener(d)}let d=(e,o)=>{e.menuItemId==t&&(chrome.tabs.sendMessage(o.id,{cmd:"html2md"}),chrome.windows.create({url:"popup.html",type:"popup",width:1024,height:768}))},m;function e(){n=!1,chrome.action.setIcon({path:"md-off.png"})}chrome.runtime.onMessage.addListener((e,o,t)=>{"load"===e.cmd?t(n):"copyMd"===e.cmd?m=e.message:"getMd"===e.cmd&&(console.log(m),t(m))});
+let enabled = false;
+//通过点击图标开启关闭插件
+chrome.action.onClicked.addListener((tab) => {
+  enabled = !enabled;
+  if (enabled) {
+    // 开启插件功能
+    open(tab.id);
+    chrome.action.setIcon({path: 'md-on.png'});
+    
+  } else {
+    // 关闭插件功能 
+    shutdown(tab.id);
+    chrome.action.setIcon({path: 'md-off.png'});
+  }
+});
+//右键菜单id
+const contextMenusId = 'html2md';
+function open(tabId){
+  chrome.contextMenus.create({
+    id: contextMenusId, 
+    title: "复制md",
+    contexts: ["all"] 
+  });
+  chrome.tabs.sendMessage(tabId, {cmd: 'open'});
+  chrome.contextMenus.onClicked.addListener(contextMenusClickHandler);
+}
+function shutdown(tabId){
+  chrome.contextMenus.remove(contextMenusId);
+  chrome.tabs.sendMessage(tabId, {cmd: 'shutdown'});
+  chrome.contextMenus.onClicked.removeListener(contextMenusClickHandler);
+}
+
+let contextMenusClickHandler = (info, tab) => {
+  
+  if(info.menuItemId == contextMenusId){
+    chrome.tabs.sendMessage(tab.id, {cmd: 'html2md'});
+    chrome.windows.create({
+      url: 'popup.html', 
+      type: 'popup',
+      width: 1024,
+      height: 768
+    });
+  }
+}
+let tempMd;
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  if(msg.cmd === 'load'){
+    sendResponse(enabled);
+  }else if(msg.cmd === 'copyMd'){
+    tempMd = msg.message;
+  }else if(msg.cmd === 'getMd'){
+    console.log(tempMd);
+    sendResponse(tempMd);
+  }
+});
+function init(){
+  enabled = false;
+  chrome.action.setIcon({path: 'md-off.png'});
+}
